b3d49ce261f9eb9b6e0fa03bf0f1157d583620dc
manoharathani27 <manoharathani2720@gmail.com>
2026-07-22 13:46:20 +0530

    Update database.js

--- api/src/config/database.js
+++ api/src/config/database.js
@@ -15,7 +15,9 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host: process.env.DB_HOST || 'localhost',
+    host: process.env.INSTANCE_CONNECTION_NAME
+    ? `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`
+    : (process.env.DB_HOST || 'localhost'),
     port: process.env.DB_PORT || 5432,
     dialect: 'postgres',
     logging: false,
